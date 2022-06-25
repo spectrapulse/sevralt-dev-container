8f1996ed732e91996126c0d3ed9bcb0a7b6ed4c8
Tyrone Faulhaber <code@spectrapulse.io>
2022-06-25 21:51:22 +0200

    Update entrypoint

--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -6,8 +6,8 @@ if [ ! -d "/root/.cfg" ]; then
   HD=/root
   
   git clone --bare https://github.com/spectrapulse/dotfiles $HD/.cfg
-  /usr/bin/git --git-dir=$HD/.cfg/ --work-tree=$HD
-  
+  /usr/bin/git --git-dir=$HD/.cfg/ --work-tree=$HD checkout -f master
+
   echo "" >> $HD/.config/starship.toml
   echo "[container]" >> $HD/.config/starship.toml
   echo "disabled = true" >> $HD/.config/starship.toml
@@ -20,7 +20,7 @@ if [ ! -d "/sevralt/.cfg" ]; then
   HD=/home/sevralt
   
   git clone --bare https://github.com/spectrapulse/dotfiles $HD/.cfg
-  /usr/bin/git --git-dir=$HD/.cfg/ --work-tree=$HD
+  /usr/bin/git --git-dir=$HD/.cfg/ --work-tree=$HD checkout -f master
   
   echo "" >> $HD/.config/starship.toml
   echo "[container]" >> $HD/.config/starship.toml
