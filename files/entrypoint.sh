78c48797c30ce3ee0f535da46a568825ac7f987a
Tyrone Faulhaber <code@spectrapulse.io>
2022-06-25 20:34:07 +0200

    Update entrypoint.sh

--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -2,6 +2,35 @@
 
 chown -R sevralt:sevralt /home/sevralt
 
+if [ ! -d "/root/.cfg" ]; then
+  HD=/root
+  
+  git clone --bare https://github.com/spectrapulse/dotfiles $HD/.cfg
+  /usr/bin/git --git-dir=$HD/.cfg/ --work-tree=$HD
+  
+  echo "" >> $HD/.config/starship.toml
+  echo "[container]" >> $HD/.config/starship.toml
+  echo "disabled = true" >> $HD/.config/starship.toml
+  echo "" >> $HD/.config/starship.toml
+  echo "[hostname]" >> $HD/.config/starship.toml
+  echo "disabled = true" >> $HD/.config/starship.toml
+fi 
+
+if [ ! -d "/sevralt/.cfg" ]; then
+  HD=/home/sevralt
+  
+  git clone --bare https://github.com/spectrapulse/dotfiles $HD/.cfg
+  /usr/bin/git --git-dir=$HD/.cfg/ --work-tree=$HD
+  
+  echo "" >> $HD/.config/starship.toml
+  echo "[container]" >> $HD/.config/starship.toml
+  echo "disabled = true" >> $HD/.config/starship.toml
+  echo "" >> $HD/.config/starship.toml
+  echo "[hostname]" >> $HD/.config/starship.toml
+  echo "disabled = true" >> $HD/.config/starship.toml
+  echo "" >> $HD/.config/starship.toml
+fi 
+
 mkdir -p /var/run/sshd
 
 /usr/sbin/sshd -D
