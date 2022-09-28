f8d7b9ab286f8dd51bb61458672469c19e7c52c0
Tyrone Faulhaber <code@spectrapulse.io>
2022-09-29 01:40:48 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,25 +1,25 @@
 #! /usr/bin/env bash
 set -e 
 
-mkdir -p /home/spectrapulse
-bindfs --force-user=spectrapulse \
-       --force-group=spectrapulse \
-       --create-for-user=1000 \
-       --create-for-group=1000 \
-       --chown-ignore \
-       --chgrp-ignore \
-       /mnt/spectrapulse \
-       /home/spectrapulse
+# mkdir -p /home/spectrapulse
+# bindfs --force-user=spectrapulse \
+#        --force-group=spectrapulse \
+#        --create-for-user=1000 \
+#        --create-for-group=1000 \
+#        --chown-ignore \
+#        --chgrp-ignore \
+#        /mnt/spectrapulse \
+#        /home/spectrapulse
 
-mkdir -p /home/sevralt
-bindfs --force-user=sevralt \
-       --force-group=sevralt \
-       --create-for-user=1001 \
-       --create-for-group=1001 \
-       --chown-ignore \
-       --chgrp-ignore \
-       /mnt/sevralt \
-       /home/sevralt
+# mkdir -p /home/sevralt
+# bindfs --force-user=sevralt \
+#        --force-group=sevralt \
+#        --create-for-user=1001 \
+#        --create-for-group=1001 \
+#        --chown-ignore \
+#        --chgrp-ignore \
+#        /mnt/sevralt \
+#        /home/sevralt
 
 yes | cp -rf /sshd_config /etc/ssh/sshd_config
 
